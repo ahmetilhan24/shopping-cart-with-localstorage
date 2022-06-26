0eb4dd5b5c6bebb75cb9d0f97805e9fe73b6f46c
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-06-26 23:12:02 +0300

    removItem func added

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,21 @@ import { STORAGE_KEY } from "./constants/storage.constants";
 let itemsData: IItem[] = [];
 
 // Add item
-const addItem = (item: IItem) => {
+const addItem = (item: IItem): void => {
   //is item
   if (!checkIsItem(item)) {
     itemsData.push(item);
   } else {
-    const index = itemsData.findIndex((elem) => elem.id === item.id);
-    itemsData.splice(index, 1);
+    removeItem(item);
   }
   setStorage();
 };
 
+//remove item
+const removeItem = (item: IItem): void => {
+  const index = itemsData.findIndex((elem) => elem.id === item.id);
+  itemsData.splice(index, 1);
+};
 // check is item
 
 const checkIsItem = (item: IItem): boolean => {
@@ -35,16 +39,17 @@ const setStorage = (): void => {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(itemsData));
 };
 
-console.log("aşlsdşlask");
-getStorage();
-//////////////// FOR DEMO
-const addBtn = document.querySelectorAll(".add-to-cart");
-addBtn.forEach((item) => {
-  item.addEventListener("click", (e: PointerEvent) => {
-    const target = e.currentTarget as HTMLElement;
-    const dataId: string = target.getAttribute("data-id");
-    if (dataId) {
-      addItem({ id: dataId });
-    }
+(() => {
+  getStorage();
+  //////////////// FOR DEMO
+  const addBtn = document.querySelectorAll(".add-to-cart");
+  addBtn.forEach((item) => {
+    item.addEventListener("click", (e: PointerEvent) => {
+      const target = e.currentTarget as HTMLElement;
+      const dataId: string = target.getAttribute("data-id");
+      if (dataId) {
+        addItem({ id: dataId });
+      }
+    });
   });
-});
+})();
