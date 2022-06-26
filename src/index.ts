8a75a90c42a95c04d6d5bcbb42a78b503a12cefd
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-06-26 20:21:23 +0300

    demo project added

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import IItem from "./types/IItem";
 import { STORAGE_KEY } from "./constants/storage.constants";
+
+const itemsData = [];
+
 // Add item
 const addItem = (item: IItem) => {
-  const oldItems = getItems();
+  const oldItems = getStorage();
   if (oldItems.length) {
     //is declared?
     const isItem: boolean = oldItems.some((elem: IItem) => elem.id === item.id);
@@ -14,13 +17,25 @@ const addItem = (item: IItem) => {
 };
 
 // Get storage
-const getItems = (): [] => {
-  const items = JSON.parse(localStorage.getItem(STORAGE_KEY));
+const getStorage = () => {
+  const items: string | null = localStorage.getItem(STORAGE_KEY);
   if (typeof items === "string") {
     return JSON.parse(items);
   }
 };
 
-const setItems = (items: IItem[] | []): void => {
+// Set item -> to localcstorage
+const setStorage = (items: IItem[] | []): void => {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(items));
 };
+
+//////////////// FOR DEMO
+const addBtn = document.querySelectorAll(".add-to-cart");
+if (addBtn instanceof HTMLElement) {
+  addBtn.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      console.log(e);
+    });
+  });
+}
+console.log(addBtn, "asljdklasj");
